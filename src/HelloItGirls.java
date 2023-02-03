a724886cb990c486227632a837269997b3d7d7c2
sandraoch1 <sandraoch2610@gmail.com>
2023-02-03 19:46:27 +0300

    first commit

---
+++ src/HelloItGirls.java
@@ -0,0 +1,17 @@
+/*public class HelloItGirls {
+    public static void main(String[] args) {
+        System.out.println("Have a good day");
+    }
+}*/
+public class HelloItGirls {
+    public static void main(String[] args) {
+        //Инициализируем переменные типа char
+        char symbol1 = 208; //По индексу символа в таблице UTF-8
+        char symbol2 = 'а'; //По значению символа
+        //char symbol3 = '\u0058'; //Через шестнадцатеричную форму Unicode
+        //Печатаем результат переменных на экран
+        System.out.println("symbol1 = " + symbol1);
+        System.out.println("symbol2 = " + symbol2);
+       // System.out.println("symbol3 = " + symbol3);
+    }
+}
